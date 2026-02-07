46d612d3386785f159c8ff0e608b20db40051ec3
jkhuertatorres-wq <jkhuertatorres@gmail.com>
2026-02-07 15:46:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-document.querySelector("button").addEventListener("click", () => {
-  document.querySelector(".about").scrollIntoView({
-    behavior: "smooth"
-  });
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 });
